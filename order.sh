25de98b98b5a04e1ed5bde1eabb391fbc508fe5b
Adam <adamjharrison123@gmail.com>
2025-11-02 17:00:13 +0000

    changed order script

--- order.sh
+++ order.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-START=0.05
-INCREMENT=0.05
+START=0.1
+INCREMENT=0.1
 END=1.60
 RUNS=5
 
@@ -13,8 +13,7 @@ do
   for (( i=1; i<=$RUNS; i++ ))
   do
     
-    ORDER_VALUE=$(python LebwohlLasher.py 1000 20 $TEMP $PARAM 1 | awk -F 'Order: ' '{print $2}' | awk '{print $1}' | tr -d ',')
-
+    ORDER_VALUE=$(mpiexec -np 8 python LebwohlLasher_mpi4py.py 1000 20 $TEMP $PARAM 1 | awk -F 'Order: ' '{print $2}' | awk '{print $1}' | tr -d ',')
     echo "$TEMP,$ORDER_VALUE"
   done
   
